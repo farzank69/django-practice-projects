302c5252dc81ba4adb3fd878512daf727ebabeb2
Farzan Khan <farzankhan61r@gmail.com>
2025-10-05 23:57:02 +0530

    Add book outlet app with initial models, views, and URL routing

--- Code/Section-4/book_outlet/models.py
+++ Code/Section-4/book_outlet/models.py
@@ -5,3 +5,5 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField()
     
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
