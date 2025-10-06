16f5dde03ba51e10681a17657b820528efdd693b
Farzan Khan <farzankhan61r@gmail.com>
2025-10-06 13:46:06 +0530

    Update admin registration and Author model

--- Code/Section-5/blog/admin.py
+++ Code/Section-5/blog/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Post, Author, Tag
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('author', 'date', 'tags') # Add filters for author, date, and tags in the admin list view.
+    prepopulated_fields = {'slug': ('title',)} # Automatically populate the slug field based on the title field.
+    list_display = ('title', 'author', 'date')
+    
+
+admin.site.register(Post, PostAdmin) # Register Post model with custom PostAdmin
 admin.site.register(Author)
 admin.site.register(Tag)
--- Code/Section-5/blog/models.py
+++ Code/Section-5/blog/models.py
@@ -14,8 +14,10 @@ class Author(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
 
-    def __str__(self):
+    def full_name(self):
         return f"{self.first_name} {self.last_name}"
+    def __str__(self):
+        return self.full_name()
 
 
 class Post(models.Model):
