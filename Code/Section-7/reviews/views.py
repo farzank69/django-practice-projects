0ad10f9aadaf0cc9eef51bb9cc046489213fbb5f
Farzan Khan <farzankhan61r@gmail.com>
2025-10-08 09:34:21 +0530

    Refactor View with different builtin Views

--- Code/Section-7/reviews/views.py
+++ Code/Section-7/reviews/views.py
@@ -3,27 +3,50 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView, CreateView
 
 from . forms import ReviewForm
 from . models import Review
 # Create your views here.
 # Generic View
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, 'reviews/review.html', {
-            'form': form
-        })
+# class ReviewView(View):
+#     def get(self, request):
+#         form = ReviewForm()
+#         return render(request, 'reviews/review.html', {
+#             'form': form
+#         })
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()                        
-            return HttpResponseRedirect('/thank-you')
+#     def post(self, request):
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             form.save()                        
+#             return HttpResponseRedirect('/thank-you')
 
-        return render(request, 'reviews/review.html', {
-            'form': form
-        })
+#         return render(request, 'reviews/review.html', {
+#             'form': form
+#         })
+
+
+#Create View 
+
+# class ReviewView(CreateView):
+#     model = Review
+#     form_class = ReviewForm
+#     template_name = "reviews/review.html"
+#     success_url = "/thank-you"
+
+
+
+#Form View
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
+
+    def form_valid(self, form):       #Additional method to save the data which has been submitted 
+        form.save()
+        return super().form_valid(form)
+    
 
 #For simple get method view, TemplateView is easy to use.
 class ThankYouView(TemplateView):
