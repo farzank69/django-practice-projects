02e3c5ec141bd3bc38af6844260575db6cc79fdb
Farzan Khan <farzankhan61r@gmail.com>
2025-10-08 07:45:15 +0530

    Initialize project with reviews app

---
+++ Code/Section-7/reviews/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.ReviewView.as_view()),      # class based view.
+    path('thank-you', views.ThankYouView.as_view())
+]
+
+
+
+
+
---
+++ Code/Section-7/reviews/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from . forms import ReviewForm
+from django.views import View
+
+# Create your views here.
+# Generic View
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, 'reviews/review.html', {
+            'form': form
+        })
+
+    def post(self, request):
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()                        
+            return HttpResponseRedirect('/thank-you')
+
+        return render(request, 'reviews/review.html', {
+            'form': form
+        })
+
+class ThankYouView(View):
+    def get(self, request):
+        return render(request, 'reviews/thank_you.html')
+
