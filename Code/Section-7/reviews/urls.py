37c3fbb5c6297dcc9d7ce50118864d82e53cfd4e
Farzan Khan <farzankhan61r@gmail.com>
2025-10-08 09:10:11 +0530

    Add ListView, TemplateView, DetailView class

--- Code/Section-7/reviews/urls.py
+++ Code/Section-7/reviews/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.ReviewView.as_view()),      # class based view.
     path('thank-you', views.ThankYouView.as_view()),
     path('review', views.ReviewListView.as_view()),
-    path('review-detail/<int:id>', views.ReviewDetailView.as_view(), name='detail')
+    path('review-detail/<int:pk>', views.ReviewDetailView.as_view(), name='detail')
 ]
 
 
--- Code/Section-7/reviews/views.py
+++ Code/Section-7/reviews/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
 
 from . forms import ReviewForm
 from . models import Review
@@ -34,22 +35,43 @@ class ThankYouView(TemplateView):
         return contex_data
 
 
-class ReviewListView(TemplateView):
-    template_name = "reviews/review_list.html"
+# class ReviewListView(TemplateView):
+#     template_name = "reviews/review_list.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         reviews = Review.objects.all()
+#         context["reviews"] = reviews
+#         return context
+
+
+# ListView will fetch the list data from the model without any unnecessary code
+class ReviewListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review            # to point it to the review model
+
+    context_object_name = "reviews"         # write own context name
+
+    # can apply the filters on the data. Example:
+    # def get_queryset(self):
+    #     query_data = super().get_queryset()
+    #     data = query_data.filter(rating__gt=4)
+    #     return data
+
     
 
-class ReviewDetailView(TemplateView):
-    template_name = "reviews/review_detail.html"
+# class ReviewDetailView(TemplateView):
+#     template_name = "reviews/review_detail.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review_id = kwargs["id"]
-        selected_data = Review.objects.get(pk=review_id)
-        context["review"] = selected_data
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         review_id = kwargs["id"]
+#         selected_data = Review.objects.get(pk=review_id)
+#         context["review"] = selected_data
+        # return context
+
+#DetailView
+class ReviewDetailView(DetailView):
+    template_name = "reviews/review_detail.html"
+    model = Review          # In the template it will use lowercase 'review' name as a context to provide all the data of the model.
+    
