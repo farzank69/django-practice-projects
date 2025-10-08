b58878bc870c3737d8edcb4ae201a87b9ec8de37
Farzan Khan <farzankhan61r@gmail.com>
2025-10-08 12:22:12 +0530

    Update forms and models to handle ImageField

--- Code/Section-8/profiles/forms.py
+++ Code/Section-8/profiles/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class ProfileForm(forms.Form):
-    user_image = forms.FileField()
+    # user_image = forms.FileField()
+    user_image = forms.ImageField()
--- Code/Section-8/profiles/views.py
+++ Code/Section-8/profiles/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from .forms import ProfileForm
+from .models import UserProfile
 # Create your views here.
 
+
+# Now the model is handling the file path so no need to save it manually. 
 # handle the file upload and store is somewhere.
-def store_file(file):
-    with open("temp/headshot.png", "wb+") as dest:      #Create and open the file
-        for chunk in file.chunks():
-            dest.write(chunk)
+# def store_file(file):
+#     with open("temp/headshot.png", "wb+") as dest:      #Create and open the file
+#         for chunk in file.chunks():
+#             dest.write(chunk)
 
 
 class CreateProfileView(View):
@@ -21,7 +24,8 @@ class CreateProfileView(View):
     def post(self, request):
         submitted_form  = ProfileForm(request.POST,request.FILES)
         if submitted_form.is_valid():
-            store_file(request.FILES["image"])
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
             return HttpResponseRedirect("/profiles")
 
         return render(request, "profiles/create_profile.html", {
